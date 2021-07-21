97b9f43bc4e338591e27825c22f4e28068a7dc76
Manish <mg7237@gmail.com>
2021-07-21 20:14:00 +0530

    UI V8

--- fx_screener/lib/screens/subscription.dart
+++ fx_screener/lib/screens/subscription.dart
@@ -35,199 +35,229 @@ class _SubscriptionPageState extends State<SubscriptionPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-          color: Color(0xffF7F8FB),
-          padding: EdgeInsets.only(
-              top: 80, left: ipad ? 25 : 15, right: ipad ? 25 : 15),
-          child: Column(children: [
-            Row(
-              children: [
-                Container(
-                    height: 25,
-                    width: 25,
-                    child: Image(
-                        image: AssetImage(
-                            'assets/icons/success-green-check-mark.png'))),
-                SizedBox(width: ipad ? 20 : 10),
-                Text("No Ads",
-                    style: TextStyle(
-                      fontSize: ipad ? 15 : 16,
-                      color: Colors.grey[600],
-                      fontFamily: "Poppins",
-                    ))
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Container(
-                    height: 25,
-                    width: 25,
-                    child: Image(
-                        image: AssetImage(
-                            'assets/icons/success-green-check-mark.png'))),
-                SizedBox(width: ipad ? 20 : 10),
-                Text("Full access to all features",
-                    style: TextStyle(
-                      fontSize: ipad ? 15 : 16,
-                      color: Colors.grey[600],
-                      fontFamily: "Poppins",
-                    ))
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Container(
-                    height: 25,
-                    width: 25,
-                    child: Image(
-                        image: AssetImage(
-                            'assets/icons/success-green-check-mark.png'))),
-                SizedBox(width: ipad ? 20 : 10),
-                Text("Fx Pattern Screener",
-                    style: TextStyle(
-                      fontSize: ipad ? 15 : 16,
-                      color: Colors.grey[600],
-                      fontFamily: "Poppins",
-                    ))
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Container(
-                    height: 25,
-                    width: 25,
-                    child: Image(
-                        image: AssetImage(
-                            'assets/icons/success-green-check-mark.png'))),
-                SizedBox(width: ipad ? 20 : 10),
-                Text("Monitoring currency fluctuation",
-                    style: TextStyle(
-                      fontSize: ipad ? 15 : 16,
-                      color: Colors.grey[600],
-                      fontFamily: "Poppins",
-                    ))
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Container(
-                    height: 25,
-                    width: 25,
-                    child: Image(
-                        image: AssetImage(
-                            'assets/icons/success-green-check-mark.png'))),
-                SizedBox(width: ipad ? 20 : 10),
-                Text("Customizable Notifications",
-                    style: TextStyle(
-                      fontSize: ipad ? 15 : 16,
-                      color: Colors.grey[600],
-                      fontFamily: "Poppins",
-                    ))
-              ],
-            ),
-            SizedBox(
-              height: ipad ? 55 : 45,
-            ),
-            Text(
-              'Get 7-Day Free Trial',
-              style: TextStyle(
-                fontFamily: "Poppins",
-                fontSize: 20 * MediaQuery.of(context).size.width / 390,
-                fontWeight: FontWeight.bold,
+    return SafeArea(
+      child: Material(
+        child: Container(
+            color: Color(0xffF7F8FB),
+            padding: EdgeInsets.only(
+                top: 30, left: ipad ? 25 : 15, right: ipad ? 25 : 15),
+            child: Column(children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Container(
+                        height: 30,
+                        width: 30,
+                        child:
+                            Image(image: AssetImage('assets/icons/tick.png'))),
+                    SizedBox(width: ipad ? 20 : 10),
+                    Text("No Ads",
+                        style: TextStyle(
+                          fontSize: ipad ? 15 : 16,
+                          color: Colors.grey[600],
+                          fontFamily: "Poppins",
+                        ))
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            InkWell(
-              child: Container(
-                  child: Text(
-                    'Subscribe',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 28 * MediaQuery.of(context).size.width / 390,
-                        letterSpacing: 1),
-                  ),
-                  padding:
-                      EdgeInsets.only(left: 50, right: 50, top: 8, bottom: 8),
-                  decoration: BoxDecoration(
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Container(
+                        height: 30,
+                        width: 30,
+                        child:
+                            Image(image: AssetImage('assets/icons/tick.png'))),
+                    SizedBox(width: ipad ? 20 : 10),
+                    Text("Full access to all features",
+                        style: TextStyle(
+                          fontSize: ipad ? 15 : 16,
+                          color: Colors.grey[600],
+                          fontFamily: "Poppins",
+                        ))
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Container(
+                        height: 30,
+                        width: 30,
+                        child:
+                            Image(image: AssetImage('assets/icons/tick.png'))),
+                    SizedBox(width: ipad ? 20 : 10),
+                    Text("Fx Patter Screener Chart",
+                        style: TextStyle(
+                          fontSize: ipad ? 15 : 16,
+                          color: Colors.grey[600],
+                          fontFamily: "Poppins",
+                        ))
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Container(
+                        height: 30,
+                        width: 30,
+                        child:
+                            Image(image: AssetImage('assets/icons/tick.png'))),
+                    SizedBox(width: ipad ? 20 : 10),
+                    Text("Monitoring currency fluctuation",
+                        style: TextStyle(
+                          fontSize: ipad ? 15 : 16,
+                          color: Colors.grey[600],
+                          fontFamily: "Poppins",
+                        ))
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  children: [
+                    Container(
+                        height: 30,
+                        width: 30,
+                        child:
+                            Image(image: AssetImage('assets/icons/tick.png'))),
+                    SizedBox(width: ipad ? 20 : 10),
+                    Text("Customizable Notifications",
+                        style: TextStyle(
+                          fontSize: ipad ? 15 : 16,
+                          color: Colors.grey[600],
+                          fontFamily: "Poppins",
+                        ))
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: ipad ? 55 : 45,
+              ),
+              Text(
+                'Get 7-Day Free Trial',
+                style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontSize: 20 * MediaQuery.of(context).size.width / 390,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              InkWell(
+                child: Container(
+                    child: Text(
+                      'Subscribe',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize:
+                              28 * MediaQuery.of(context).size.width / 390,
+                          letterSpacing: 1),
+                    ),
+                    padding:
+                        EdgeInsets.only(left: 50, right: 50, top: 8, bottom: 8),
+                    decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(50),
-                      gradient: LinearGradient(
-                          colors: _listColors,
-                          stops: [0.1, 0.5, 0.9],
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter))),
-              onTap: () async {
-                //widget.purchase();
-              },
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              "Then \$9.99 USD/Month",
-              style: TextStyle(
-                fontFamily: "Poppins",
-                fontSize: 20 * MediaQuery.of(context).size.width / 390,
-                fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    )),
+                onTap: () async {
+                  //widget.purchase();
+                },
               ),
-            ),
-            SizedBox(height: 40),
-            GestureDetector(
-              onTap: () async {
-                // if (widget.isSubscriptionActive) {
-                print("True");
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => HomePage()));
-              }
-              //   // } else {
-              //     print("False");
-              //     AlertDialog(
-              //       elevation: 10,
-              //       shape: RoundedRectangleBorder(
-              //           borderRadius: BorderRadius.all(Radius.circular(20.0))),
-              //       title: Text("NO_SUBSCRIPTION"),
-              //       content: Text(
-              //           "Invalid subscription, please subscribe to get access to the APP content"),
-              //     );
-              //   }
-              // },
-              ,
-              child: Text("Restore Purchases",
-                  style: TextStyle(
-                      color: Colors.blue,
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Then \$9.99 USD/Month",
+                style: TextStyle(
+                  fontFamily: "Poppins",
+                  fontSize: 20 * MediaQuery.of(context).size.width / 390,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 40),
+              GestureDetector(
+                onTap: () async {
+                  // if (widget.isSubscriptionActive) {
+                  print("True");
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => HomePage()));
+                }
+                //   // } else {
+                //     print("False");
+                //     AlertDialog(
+                //       elevation: 10,
+                //       shape: RoundedRectangleBorder(
+                //           borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                //       title: Text("NO_SUBSCRIPTION"),
+                //       content: Text(
+                //           "Invalid subscription, please subscribe to get access to the APP content"),
+                //     );
+                //   }
+                // },
+                ,
+                child: Text("Restore Purchases",
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: ipad
+                            ? 13 * MediaQuery.of(context).size.width / 390
+                            : 15 * MediaQuery.of(context).size.width / 390,
+                        letterSpacing: 0.1)),
+              ),
+              SizedBox(
+                height: ipad ? 50 : 30,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Center(
+                  child: Text(
+                    "After 7 days free trial, payment will be charged \$9.99 USD automatically each month to your Apple ID account unless it is cancelled no later than 24 hours prior to the renewal date. You can cancel automatic renewal or manage your subscription anytime by going to your Account Settings on the App Store.",
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                      fontFamily: "Poppins",
+                      color: Colors.grey[600],
                       fontSize: ipad
-                          ? 13 * MediaQuery.of(context).size.width / 390
-                          : 15 * MediaQuery.of(context).size.width / 390,
-                      letterSpacing: 0.1)),
-            ),
-            SizedBox(
-              height: ipad ? 50 : 30,
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Center(
-                child: Text(
-                  "After 7 days free trial, payment will be charged \$9.99 USD automatically each month to your Apple ID account unless it is cancelled no later than 24 hours prior to the renewal date. You can cancel automatic renewal or manage your subscription anytime by going to your Account Settings on the App Store.",
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontFamily: "Poppins",
-                    color: Colors.grey[600],
-                    fontSize: ipad
-                        ? 11 * MediaQuery.of(context).size.width / 390
-                        : 12 * MediaQuery.of(context).size.width / 390,
-                    fontWeight: FontWeight.bold,
+                          ? 11 * MediaQuery.of(context).size.width / 390
+                          : 12 * MediaQuery.of(context).size.width / 390,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ])),
+            ])),
+      ),
     );
   }
 }
